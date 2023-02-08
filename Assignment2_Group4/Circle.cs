2a98a4ec8c251eb8aba01fd317630e3fcf7444f3
ravalikrishna5121 <ravalikrishna1996@gmail.com>
2023-02-08 12:29:07 -0500

    Containing class

---
+++ Assignment2_Group4/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using NUnit;
+
+
+namespace Assignment2_Group4
+{
+    public class Circle
+    {
+        private int pvtradius { get; set; }
+        public int InputRadius;
+        public Circle() //default constructor
+        {
+            pvtradius = 1;
+        }
+
+        // Parameterised Constructor
+        public Circle(int InputRadius)
+        {
+            pvtradius = InputRadius;
+        }
+        public int GetRadius()
+        {
+            return pvtradius;
+        }
+
+        public void SetRadius(int inputRadius)
+        {
+            pvtradius = inputRadius;
+        }
+
+        public double GetCircumference()
+        {
+
+
+            return 2 * Math.PI * this.pvtradius;
+
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * pvtradius * pvtradius;
+        }
+
+    }
+}
--- Assignment2_Group4/Program.cs
+++ Assignment2_Group4/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using NUnit;
 
 namespace Assignment2_Group4
 {
@@ -6,7 +9,85 @@ namespace Assignment2_Group4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int radius = 0;
+            bool flag = false;
+
+            do
+            {
+                Console.Write("Enter the radius of your choice\n");
+                try
+                {
+                    radius = Convert.ToInt32(Console.ReadLine());
+                    if (radius < 1)
+                    {
+                        Console.WriteLine("Radius with 0 and less than 0 are not valid");
+
+                    }
+                    else
+                    {
+                        flag = true;
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("Radius value cannot be a non numeric value");
+                }
+
+            } while (flag != true);
+
+
+
+            Circle circle = new Circle(radius);
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("1. Get Circle Radius");
+                    Console.WriteLine("2. Change Circle Radius");
+                    Console.WriteLine("3. Get Circle Circumference");
+                    Console.WriteLine("4. Get Circle Area");
+                    Console.WriteLine("5. Exit");
+
+                    Console.Write("\nPlease select from the options available\n");
+                    int selection = Convert.ToInt32(Console.ReadLine());
+                    if (selection == 1)
+                    {
+                        Console.WriteLine("The Radius of the circle is " + circle.GetRadius() + "\n");
+                    }
+                    else if (selection == 2)
+                    {
+                        Console.Write("Enter the new radius of the circle\n");
+                        int newRadius = Convert.ToInt32(Console.ReadLine());
+                        circle.SetRadius(newRadius);
+                    }
+                    else if (selection == 3)
+                    {
+                        Console.WriteLine("Circumference of the circle is " + circle.GetCircumference() + "\n");
+                    }
+                    else if (selection == 4)
+                    {
+                        Console.WriteLine("Area of the circle is " + circle.GetArea() + "\n");
+                    }
+                    else if (selection == 5)
+                    {
+                        Console.WriteLine("Exit program");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Select from the available menu options\n");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an interger value for the option !! ");
+                }
+            }
+
+
         }
     }
 }
+
+
